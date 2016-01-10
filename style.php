3aabfd4b7714a0249f0092bfcdcdc6d4fe1ada6e
AnthonyHaffey <anthony.haffey@gmail.com>
2016-01-10 15:33:32 +0000

    Working: but missing backup and multiple log-ins
    As described in title

---
+++ style.php
@@ -0,0 +1,101 @@
+<style>
+	h1{
+		margin: auto;
+		width: 50%;
+		padding: 10px;
+	}
+	#saveClick{
+		float:right;
+	}
+	body{
+		margin: auto;
+		width: 80%;
+		padding: 10px;		
+	}
+	.leftCol{
+		width:400px;
+	}
+	.middleCol{
+		width:75px;
+	}
+	.rightCol{
+		width:10%;
+	}
+	.job{
+		background-color:#90C3D4;
+		width:100%;
+	}
+	.job:hover{
+		background-color:green;
+	}
+		.person{
+		background-color:#90C3D4;
+		width:100%;
+	}
+	.person:hover{
+		background-color:green;
+	}
+	.job:focus{
+		background-color:green;
+		color:white;
+	}
+	.date{
+		width:100%;
+	}
+	.date:hover{
+		background-color:green;
+	}
+	.date:focus{
+		background-color:green;
+	}
+	.dateEarly{
+		background-color:white;
+	}
+	.dateDue{
+		background-color:#90C3D4;
+	}
+	.dateLate{
+		background-color:red;
+	}
+	#jobTags{
+		absolute:right;
+		width
+	}
+	.treeButton {
+		font-family:"Open Sans", "open_sans_local", Helvetica, Arial, sans-serif;
+		text-align:center;
+		padding:.25em .4em .45em .4em;
+		color:#fff;
+		background-color:#006600;
+		border-radius:4px;
+		border:0;
+		cursor: pointer;
+	}
+	.treeButton:hover{
+		background-color:green;
+		color:black;
+	}
+	.treeButtonSel {
+		font-family:"Open Sans", "open_sans_local", Helvetica, Arial, sans-serif;
+		text-align:center;
+		padding:.25em .4em .45em .4em;
+		color:#fff;
+		background-color:#282F9E;
+		border-radius:4px;
+		border:0;
+		cursor: pointer;
+	}
+	.treeButtonSel:hover{
+		background-color:blue;
+		color:black;
+	}
+	textarea {
+		resize: none;
+	}
+	#loginDiv{
+		margin: auto;
+		width: 40%;
+		padding: 10px;
+	}
+
+</style>
